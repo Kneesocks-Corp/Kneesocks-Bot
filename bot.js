1fb99e140a2348c0f6db007ac2d57b6cc7bde8b2
LunaHaxxor <JustSweetLuna@gmail.com>
2020-03-23 13:51:27 +0100

    huh

--- bot.js
+++ bot.js
@@ -2,8 +2,11 @@ const botSettings = require("./botsettings.json");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.om('ready', () => {
+client.once('ready', () => {
     console.log('Ready!');
 });
 
-bot.login(botSettings.token);
+client.on('message', message => {
+    console.log(message.content);
+});
+client.login(botSettings.token);
