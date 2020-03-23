5798547e79bd71e000abc12dd90a085df92d0c2a
LunaHaxxor <JustSweetLuna@gmail.com>
2020-03-23 13:46:14 +0100

    fix

--- bot.js
+++ bot.js
@@ -1,1 +1,9 @@
 const botSettings = require("./botsettings.json");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(botSettings.token);
