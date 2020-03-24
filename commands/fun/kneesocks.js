8c143d14b54d09055566fff6170ed2d4b9916cdc
LunaHaxxor <JustSweetLuna@gmail.com>
2020-03-24 21:54:56 +0100

    Fixed kneesocks

--- commands/fun/kneesocks.js
+++ commands/fun/kneesocks.js
@@ -3,6 +3,7 @@ const {
     Command
 } = require('discord.js-commando');
 const Danbooru = require('danbooru');
+const axious = require('axios');
 const booru = new Danbooru();
 
 module.exports = class DanbooruCommand extends Command {
@@ -16,20 +17,24 @@ module.exports = class DanbooruCommand extends Command {
         })
     }
     run(msg) {
-        booru.posts({
-            random: "true",
-            tags: 'rating:safe kneehighs order:date'
-        }).then(posts => {
-            const index = Math.floor(Math.random() * posts.length);
-            const post = posts[index];
-
-            const url = booru.url(post.large_file_url)
-            msg.react("👍")
+        async function getRKS() {
+            const res = await axios.get(`https://kneesocks.now.sh/api/v2/RKS.json`);
+            if (res.length == 0) {
+                return false;
+            } else {
+                return res.data.data;
+            }
+        };
+        getRKS().then(result => {
+            const imageStream = Buffer.from(result.split(',')[1], 'base64');
+            const attachment = new Discord.MessageAttachment(imageStream);
+            msg.react("??")
             msg.delete(1500)
             const embed = new Discord.RichEmbed()
                 .setDescription("Here are random Kneesocks")
                 .setColor('#FFFFFF')
-                .setImage(url)
+                .attachFiles(attachment)
+                .setImage('attachment://file.jpg')
                 .setFooter(`Requested by ${msg.member.displayName}`)
                 .setTimestamp();
             msg.channel.send(embed);
