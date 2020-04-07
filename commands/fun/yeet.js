4f8053c3453b3fc8cacf4c3f7fe221f628fb8da4
LunaHaxxor <JustSweetLuna@gmail.com>
2020-04-08 01:59:01 +0200

    test

---
+++ commands/fun/yeet.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const {
+    Command
+} = require('discord.js-commando');
+const Danbooru = require('danbooru');
+const booru = new Danbooru();
+
+module.exports = class DanbooruCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'yeet',
+            memberName: 'yeet',
+            group: 'fun',
+            description: 'Sends a random Safe for Work Ahegao Image from Danbooru',
+            guarded: true,
+        })
+    }
+    run(msg) {
+        msg.react("đ")
+        msg.delete(1500)
+        msg.channel.send("Im in " + `${client.guilds.size} servers!`);
+    }
+}
