64ffc77964e5e51b2da774a0d7aaf51246d3d0e8
LunaHaxxor <JustSweetLuna@gmail.com>
2020-04-08 01:54:43 +0200

    deleted smth

--- test.js
+++ test.js
@@ -33,15 +33,15 @@ const activities_list = [
     "with the *help command.",
     "with kneesocks",
     "with Akira <3",
-    "Im in " + client.guilds.size,
     "with Danbooru API"
 ]; // creates an arraylist containing phrases you want your bot to switch through.
 
-client.on('ready', () => {
+client.on("ready", () => {
     setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 200000); // Runs this every 10 seconds.
+        let activities = [activities_list, `${client.guilds.size} servers!`],
+            index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+        client.user.setActivity(activities[index])
+    }, 5000);
 });
 
 client.login(process.env.token);
