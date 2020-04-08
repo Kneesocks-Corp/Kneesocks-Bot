53d1d9c8f8d17f794799d4a925095ee148f1bb7c
LunaHaxxor <JustSweetLuna@gmail.com>
2020-04-08 02:44:55 +0200

    Yeet

--- test.js
+++ test.js
@@ -47,7 +47,7 @@ client.on("ready", () => {
 });
 
 client.on('message', message => {
-    if (message.content === '*yeet') {
+    if (message.content === '*guilds') {
         message.react("đ")
         message.delete(1500)
         message.channel.send("Im in " + [`${client.guilds.size}`] + " Servers");
