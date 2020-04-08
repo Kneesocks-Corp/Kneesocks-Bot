7df2a0f6d7b8b82e4f9f99b6b998de574ca1ba44
LunaHaxxor <JustSweetLuna@gmail.com>
2020-04-08 02:10:14 +0200

    fix

--- test.js
+++ test.js
@@ -38,14 +38,14 @@ const activities_list = [
 
 client.on("ready", () => {
     setInterval(() => {
-        let activities = ["Im in", `${client.guilds.size} servers!`],
+        let activities = ["", `in ${client.guilds.size} servers!`],
             index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
         client.user.setActivity(activities[index])
     }, 5000);
 });
 
 client.on('message', message => {
-    if (message.content === bot.prefix + '*yeet') {
+    if (message.content === '*yeet') {
         message.react("đ")
         message.delete(1500)
         message.channel.send("Im in ", `${bot.guilds.size} servers!`);
